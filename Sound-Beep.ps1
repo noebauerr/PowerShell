292d157274d2e55e7433a0055915b77d87c69b9e
Reinhard <51676396+noebauerr@users.noreply.github.com>
2021-04-08 19:49:17 +0200

    Update Sound-Beep.ps1

--- Sound-Beep.ps1
+++ Sound-Beep.ps1
@@ -1,5 +1,5 @@
 ﻿# Einen Ton abspielen
-# der erste Parameter gibt die Tonhöhe an, der zweite Parameter die Länge
+# der erste Parameter gibt die Tonhoehe an, der zweite Parameter die Länge
 [Console]::Beep(500,100)
 [Console]::Beep(600,100)
 [Console]::Beep(700,300)
