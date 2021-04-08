bdabfd9a334b229a3b2de14d5bfd43d35f660203
Reinhard <51676396+noebauerr@users.noreply.github.com>
2021-04-08 19:40:05 +0200

    Umlaut im Kommentar entfernt

--- VM-Bluescreen.ps1
+++ VM-Bluescreen.ps1
@@ -1,4 +1,4 @@
-# bösen VM-BlueScreen erzeugen
+# boesen VM-BlueScreen erzeugen
 # dieser Befehl erzeugt in den VMs einen Bluescreen (Stand Juni 2020)
 
 get-vm | Out-GridView -PassThru | debug-vm -InjectNonMaskableInterrupt
