60b2a960dcc2b3a9233dd8b123bf64f0be53f3fb
Reinhard <51676396+noebauerr@users.noreply.github.com>
2021-04-12 19:38:15 +0200

    Update VM-Bluescreen.ps1

--- VM-Bluescreen.ps1
+++ VM-Bluescreen.ps1
@@ -1,4 +1,5 @@
 # boesen VM-BlueScreen erzeugen
 # dieser Befehl erzeugt in den VMs einen Bluescreen (Stand Juni 2020)
+# Der folgende Befehl muss am Hyper-V Host ausgefuehrt werden
 
 get-vm | Out-GridView -PassThru | debug-vm -InjectNonMaskableInterrupt
