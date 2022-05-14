16fa10c6dcd6c9f17a9c3fd7db6c2a433c8c2711
Reinhard <51676396+noebauerr@users.noreply.github.com>
2022-05-14 17:22:44 +0200

    Update Exchange Notizen.ps1

--- Exchange/Exchange Notizen.ps1
+++ Exchange/Exchange Notizen.ps1
@@ -12,3 +12,48 @@ Start-Process https://www.frankysweb.de/exchange-monitor/       # Exchange Monit
 # folgende Zeile als Admin ausfuehren, ist fuer die ISE Integration notwendig
 Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
 
+$servername = "v-exchange"  # Hostname vom Exchange Server
+
+# EventID 15004 - Healthstatus erhöht
+# EventID 15005 - Status normalisiert
+# EventID 15006 - Speicherplatz wird knapp
+# EventID 15007 - RAMauslastung zu HOCH
+
+Get-EventLog -LogName system | ? eventid -EQ 15007
+
+
+Get-OwaVirtualDirectory -Server $servername | fl *
+get-clientaccessservice * | fl *
+
+Get-OwaVirtualDirectory -Server $servername
+Get-EcpVirtualDirectory -server $servername
+Get-WebServicesVirtualDirectory -server $servername
+Get-ActiveSyncVirtualDirectory -Server $servername
+Get-OabVirtualDirectory -Server $servername
+Get-MapiVirtualDirectory -Server $servername
+Get-OutlookAnywhere -Server $servername
+
+get-mailbox | Get-Member
+
+Get-Mailbox | sort servername | ft name, servername, haspicture
+
+
+# die wichtigsten Abfragen zu den Usermailboxen am Server
+
+# Name und Größe und Anzahl wird angezeigt, sortiert nach Größe (über 1 GB)
+Get-MailboxStatistics -Server v-exchange |sort totalitemsize -descending |? totalitemsize -gt 1000MB | ft displayname, totalitemsize, itemcount
+
+# der Spitzenreiter nach Anzahl der Mails (über 9999 Stück)
+Get-MailboxStatistics -Server v-exchange |sort itemcount -descending | ? itemcount -gt 9999 | ft displayname, itemcount, totalitemsize
+
+# storagelimitstatus erreicht
+get-mailboxstatistics -server v-exchange | sort storagelimitstatus -descending | ft displayname, storagelimitstatus
+
+#E-Mail Adressen aller Mailbox Datenbanken auslesen:
+#Get-MailboxDatabase | Get-Mailbox | fl DisplayName, EmailAddresses > c:\Mailbox.csv
+Get-MailboxDatabase | Get-Mailbox | ft DisplayName, EmailAddresses
+
+#E-Mail Adressen aus einer öffentlichen Ordner Datenbank auslesen:
+#Get-MailPublicFolder | fl Displayname, EmailAddresses > c:\PublicFolder.csv
+Get-MailPublicFolder | ft Displayname, EmailAddresses
+
