da7e04b51d23b643f39d31e6bad532d0c2cf3c68
Reinhard <51676396+noebauerr@users.noreply.github.com>
2022-05-14 17:21:23 +0200

    Create Exchange Notizen.ps1
    Exchange Notizen

---
+++ Exchange/Exchange Notizen.ps1
@@ -0,0 +1,14 @@
+# Exchange HealthChecker.ps1 von Microsoft
+Start-Process https://microsoft.github.io/CSS-Exchange/Diagnostics/HealthChecker/
+Start-Process https://github.com/microsoft/CSS-Exchange/releases/latest/download/HealthChecker.ps1
+
+# Exchange Infos und Tools
+Start-Process https://www.frankysweb.de/
+Start-Process https://www.frankysweb.de/exchange-reporter-2013/ # Exchange Reporter
+Start-Process https://www.frankysweb.de/message-tracking-gui/   # Exchange Message Tracking GUI
+Start-Process https://www.frankysweb.de/exchange-monitor/       # Exchange Monitor
+
+
+# folgende Zeile als Admin ausfuehren, ist fuer die ISE Integration notwendig
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
+
