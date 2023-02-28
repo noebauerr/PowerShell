b2347d386183e549779a5872630d8de7c0ebdb06
Reinhard <51676396+noebauerr@users.noreply.github.com>
2023-02-28 09:25:05 +0100

    Add files via upload
    Microsoft Updates mit der PowerShell aktivieren

---
+++ Microsoft Updates.ps1
@@ -0,0 +1,13 @@
+﻿# Microsoft Updates abfragen (geht als User) bzw aktivieren (geht nur als Admin)
+# hier sind wirklich die Microsoft Updates (auch Office,...) und nicht die Windows Updates gemeint
+
+
+# zuerst die Update-Methode "initialisieren"
+$mu = New-Object -ComObject Microsoft.Update.ServiceManager -Strict
+
+# Abfrage ob Microsoft Updates aktiviert (true) ist
+$mu.services | ? name -eq "Microsoft Update" | select IsRegisteredWithAU
+
+
+# die Aktivierung benoetigt lokale Administrator Rechte
+$mu.RegisterServiceWithAU("7971f918-a847-4430-9279-4a52d1efe18d")
