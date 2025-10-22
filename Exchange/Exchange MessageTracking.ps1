71207713f1ad85966be1710de70de6960dfdded7
Reinhard <51676396+noebauerr@users.noreply.github.com>
2025-10-22 16:06:50 +0200

    .

---
+++ Exchange/Exchange MessageTracking.ps1
@@ -0,0 +1,11 @@
+﻿Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn  # fuer PowerShell ISE notwendig
+
+$jetzt = get-date
+$gestern = (Get-Date).AddDays(-1)
+
+Get-MessageTrackingLog -ResultSize Unlimited -start $gestern -end $jetzt -EventId Fail
+Get-MessageTrackingLog -ResultSize Unlimited -start $gestern -end $jetzt -EventId Fail | fl *
+
+Get-MessageTrackingLog -ResultSize Unlimited -start $gestern -end $jetzt -Sender administrator@test.at
+
+get-help Get-MessageTrackingLog -full
---
+++ noch einsortieren/grauer Hintergrund.ps1
@@ -0,0 +1,5 @@
+﻿# Leeren des aktuellen Hintergrundbilds
+Set-ItemProperty "HKCU:\Control Panel\Desktop" -Name Wallpaper -Value ""
+
+# Den Farbwert für Grau setzen (hier: 128 128 128 als Beispiel)
+Set-ItemProperty "HKCU:\Control Panel\Colors" -Name Background -Value "128 128 128"
